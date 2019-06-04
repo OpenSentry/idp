6be829279fc1266a2a07728707b5eb63fd8a3922
Lasse Nielsen <>
2019-06-04 10:18:08 +0200

    Changed interface specs to go files.. Seems to be a better way since we can use govalidator and get some kind of documentation at the same time

---
+++ interface-specs/identities-recover.go
@@ -0,0 +1,9 @@
+package interfaces
+
+type PostIdentitiesRecoverRequest struct {
+  Id      int64
+}
+
+type PostIdentitiesRecoverResponse struct {
+  Id      int64
+}
---
+++ interface-specs/identities.go
@@ -0,0 +1,25 @@
+package interfaces
+
+type GetIdentitiesRequest struct {
+  Id      int64
+}
+
+type GetIdentitiesResponse struct {
+  Id      int64
+}
+
+type PostIdentitiesRequest struct {
+  Id      int64
+}
+
+type PostIdentitiesResponse struct {
+  Id      int64
+}
+
+type PutIdentitiesRequest struct {
+  Id      int64
+}
+
+type PutIdentitiesResponse struct {
+  Id      int64
+}
