379e27672b5ba0bffdb9eaf4a2ef0b1718fdb0ce
Lasse Roed Nielsen <larn@fullrate.dk>
2019-06-14 10:24:31 +0200

    Removed annoying windows encodings and changed project name around the code to work with git clone

--- controller/v1/identities.go
+++ controller/v1/identities.go
@@ -1,50 +1,49 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"idp/gateway/db"
-) 
+  "github.com/gin-gonic/gin"
+  "net/http"
+  "golang-idp-be/gateway/db"
+)
 
 func GetIdentities(c *gin.Context) {
+  result, err := db.HelloWorld("bolt://neo4j", "neo4j", "test")
 
-	result, err := db.HelloWorld("bolt://neo4j", "neo4j", "test")
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": result,
-		"error": err,
-	})
+  c.JSON(http.StatusOK, gin.H{
+          "message": result,
+          "error": err,
+  })
 }
 
 func PostIdentities(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+  c.JSON(http.StatusOK, gin.H{
+    "message": "pong",
+  })
 }
 
 func PutIdentities(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+  c.JSON(http.StatusOK, gin.H{
+    "message": "pong",
+  })
 }
 
 func GetIdentitiesAuthenticate(c *gin.Context) {
-	id := c.Query("id")
-	password := c.Query("password")
-	message := "id: " + id + ", password: " + password
-	c.JSON(http.StatusOK, gin.H{
-		"message": message,
-	})
+  id := c.Query("id")
+  password := c.Query("password")
+  message := "id: " + id + ", password: " + password
+  c.JSON(http.StatusOK, gin.H{
+    "message": message,
+  })
 }
 
 func GetIdentitiesRevoke(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+  c.JSON(http.StatusOK, gin.H{
+    "message": "pong",
+  })
 }
 
 func GetIdentitiesRecover(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+  c.JSON(http.StatusOK, gin.H{
+    "message": "pong",
+  })
 }
