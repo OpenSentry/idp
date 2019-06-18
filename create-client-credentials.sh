ab8054a9f47e173e97461974d1902c664e0b5733
Marc <wraix@users.noreply.github.com>
2019-06-18 08:46:16 +0200

    corrections

--- create-client-credentials.sh
+++ create-client-credentials.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
+CLIENT_ID=idp-be
 FILE=.idp-be.yml
 
-if [ -f ".idp-be.json" ]
+if [ -f "$FILE" ]
 then
   echo "Config file $FILE already exists"
   exit 1
@@ -12,14 +13,14 @@ fi
 HYDRA_ADMIN_URL="http://hydra:4445"
 HYDRA_DOCKER_NETWORK="sso-examples_trusted"
 
-# Create client credentials for idp-be service
+# Create client credentials for service
 cmd=$(docker run --rm -it \
   -e HYDRA_ADMIN_URL="${HYDRA_ADMIN_URL}"\
   --network "${HYDRA_DOCKER_NETWORK}" \
   oryd/hydra \
   clients create \
     --skip-tls-verify \
-    --id idp-be \
+    --id $CLIENT_ID \
     --name identity-provider-backend \
     --grant-types client_credentials \
     --response-types token \
@@ -28,8 +29,8 @@ cmd=$(docker run --rm -it \
 if [ "$?" -eq 0 ]
 then
 SECRET=$(echo -n "$cmd" | grep Secret: | sed 's/^.*\(Secret:.*\)/\1/g' | awk '{ printf $2 }' )
-echo "client_id: idp-be" > $FILE
-echo "client_secret: "$SECRET >> .idp-be.yml
+echo "client_id: $CLIENT_ID" > $FILE
+echo "client_secret: $SECRET" >> $FILE
 chmod 400 $FILE
 else
   echo "Error: $?"
