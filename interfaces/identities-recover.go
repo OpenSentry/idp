e587a82badc2eac4e72b8fe4cd97e100322b0862
Lasse Nielsen <charmixer@users.noreply.github.com>
2019-06-19 08:46:32 +0200

    Ugly but working flow for hydra

---
+++ interfaces/identities-recover.go
@@ -0,0 +1,11 @@
+package interfaces
+
+type PostIdentitiesRecoverRequest struct {
+  Id              string            `json:"id"`
+}
+
+type PostIdentitiesRecoverResponse struct {
+  Id              string          `json:"id"`
+  Email           string          `json:"email"`
+  RecoverMethod   string          `json:"recover_method"`
+}
