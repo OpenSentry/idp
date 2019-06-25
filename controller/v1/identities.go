b47075f1bd7ed8520ecf3bff2a998d58bba7ce73
Marc <wraix@users.noreply.github.com>
2019-06-25 10:02:18 +0200

    Added /userinfo

--- controller/v1/identities.go
+++ controller/v1/identities.go
@@ -9,8 +9,21 @@ import (
 )
 
 func GetIdentities(c *gin.Context) {
-  c.JSON(http.StatusOK, gin.H{
-    "message": "pong",
+
+  id, _ := c.GetQuery("id")
+
+  if id == "user-1" {
+    c.JSON(http.StatusOK, gin.H{
+      "id": id,
+      "name": "Test bruger",
+      "email": "test@test.dk",
+    })
+    return
+  }
+
+  // Deny by default
+  c.JSON(http.StatusNotFound, gin.H{
+    "error": "Not found",
   })
 }
 
