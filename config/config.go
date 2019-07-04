d462fedae266411833a7250848edb3bc15eb3527
Marc <wraix@users.noreply.github.com>
2019-07-04 23:06:30 +0200

    oidc and closure for handlers

--- config/config.go
+++ config/config.go
@@ -4,46 +4,49 @@ import (
   "os"
 )
 
-/*
-RedirectURL:  redirect url,
-ClientID:     "GOOGLE_CLIENT_ID",
-ClientSecret: "CLIENT_SECRET",
-Scopes:       []string{"scope1", "scope2"},
-Endpoint:     oauth2 endpoint,
-*/
-
 type HydraConfig struct {
-  Url                       string
-  AdminUrl                  string
+  Url             string
+  AdminUrl        string
+  AuthenticateUrl string
+  TokenUrl        string
+  UserInfoUrl     string
+
   LoginRequestUrl           string
   LoginRequestAcceptUrl     string
   LoginRequestRejectUrl     string
+
   LogoutRequestUrl           string
   LogoutRequestAcceptUrl    string
   LogoutRequestRejectUrl    string
-  UserInfoUrl               string
 }
 
-type OAuth2ClientConfig struct {
-  ClientId        string
-  ClientSecret    string
-  Scopes          []string
-  RedirectURL     string
-  Endpoint        string
+type IdpBeConfig struct {
+  ClientId string
+  ClientSecret string
+  RequiredScopes []string
 }
 
 var Hydra HydraConfig
+var IdpBe IdpBeConfig
 
 func InitConfigurations() {
   Hydra.Url                   = getEnvStrict("HYDRA_URL")
   Hydra.AdminUrl              = getEnvStrict("HYDRA_ADMIN_URL")
+  Hydra.AuthenticateUrl       = Hydra.Url + "/oauth2/auth"
+  Hydra.TokenUrl              = Hydra.Url + "/oauth2/token"
+  Hydra.UserInfoUrl           = Hydra.Url + "/userinfo"
+
   Hydra.LoginRequestUrl       = Hydra.AdminUrl + "/oauth2/auth/requests/login"
   Hydra.LoginRequestAcceptUrl = Hydra.LoginRequestUrl + "/accept"
   Hydra.LoginRequestRejectUrl = Hydra.LoginRequestUrl + "/reject"
+
   Hydra.LogoutRequestUrl       = Hydra.AdminUrl + "/oauth2/auth/requests/logout"
   Hydra.LogoutRequestAcceptUrl = Hydra.LogoutRequestUrl + "/accept"
   Hydra.LogoutRequestRejectUrl = Hydra.LogoutRequestUrl + "/reject"
-  Hydra.UserInfoUrl            = Hydra.Url + "/userinfo"
+
+  IdpBe.ClientId              = getEnvStrict("IDP_BACKEND_OAUTH2_CLIENT_ID")
+  IdpBe.ClientSecret          = getEnvStrict("IDP_BACKEND_OAUTH2_CLIENT_SECRET")
+  IdpBe.RequiredScopes        = []string{"openid"}
 }
 
 func getEnv(name string) string {
