b170070b04cc453c742210b633ae7b08d01e3912
Marc <wraix@users.noreply.github.com>
2019-07-04 23:06:30 +0200

    oidc and closure for handlers

--- controller/identities.go
+++ controller/identities.go
@@ -5,11 +5,47 @@ import (
   "fmt"
   "net/http"
 
+  _ "golang.org/x/net/context"
+  _ "golang.org/x/oauth2"
+  "golang.org/x/oauth2/clientcredentials"
+
+  oidc "github.com/coreos/go-oidc"
   "github.com/gin-gonic/gin"
 
   _ "golang-idp-be/config"
 )
 
+type IdpBeEnv struct {
+  Provider *oidc.Provider
+  HydraConfig *clientcredentials.Config
+}
+
+func GetIdentities(env *IdpBeEnv) gin.HandlerFunc {
+  fn := func(c *gin.Context) {
+    fmt.Println(fmt.Sprintf("[request-id:%s][event:GetIdentities]", c.MustGet("RequestId")))
+
+    id, _ := c.GetQuery("id")
+
+    if id == "user-1" {
+      c.JSON(http.StatusOK, gin.H{
+        "id": id,
+        "name": "Test bruger",
+        "email": "test@test.dk",
+      })
+      c.Abort()
+      return
+    }
+
+    // Deny by default
+    c.JSON(http.StatusNotFound, gin.H{
+      "error": "Not found",
+    })
+    c.Abort()
+  }
+  return gin.HandlerFunc(fn)
+}
+
+/*
 func GetIdentities(c *gin.Context) {
   fmt.Println(fmt.Sprintf("[request-id:%s][event:GetIdentities]", c.MustGet("RequestId")))
 
@@ -30,7 +66,7 @@ func GetIdentities(c *gin.Context) {
     "error": "Not found",
   })
   c.Abort()
-}
+}*/
 
 func PostIdentities(c *gin.Context) {
   c.JSON(http.StatusOK, gin.H{
--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
   "fmt"
   "strings"
   "net/http"
+  "net/url"
+  
 
+  "golang.org/x/net/context"
   "golang.org/x/oauth2"
+  "golang.org/x/oauth2/clientcredentials"
 
+  oidc "github.com/coreos/go-oidc"
   "github.com/gin-gonic/gin"
   "github.com/atarantini/ginrequestid"
+
 
   "golang-idp-be/config"
   "golang-idp-be/controller"
@@ -20,11 +26,33 @@ func init() {
 
 func main() {
 
+  provider, err := oidc.NewProvider(context.Background(), config.Hydra.Url + "/")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Setup hydra config. Used for Authorization code flow. (should this go into idpbe?)
+  hydraConfig := &clientcredentials.Config{
+    ClientID:     config.IdpBe.ClientId,
+    ClientSecret: config.IdpBe.ClientSecret,
+    TokenURL:     config.Hydra.TokenUrl,
+    Scopes:       config.IdpBe.RequiredScopes,
+    EndpointParams: url.Values{"audience": {"hydra"}},
+    AuthStyle: 2, // https://godoc.org/golang.org/x/oauth2#AuthStyle
+  }
+
+  // Setup app state variables. Can be used in handler functions by doing closures see exchangeAuthorizationCodeCallback
+  env := &controller.IdpBeEnv{
+    Provider: provider,
+    HydraConfig: hydraConfig,
+  }
+
   r := gin.Default()
   r.Use(ginrequestid.RequestId())
   //r.Use(logRequest())
   r.Use(requireBearerAccessToken())
-  r.GET( "/identities", controller.GetIdentities)
+  r.GET( "/identities", controller.GetIdentities(env))
   r.POST("/identities", controller.PostIdentities)
   r.PUT( "/identities", controller.PutIdentities)
   r.POST( "/identities/authenticate", controller.PostIdentitiesAuthenticate)
@@ -32,7 +60,8 @@ func main() {
   r.POST( "/identities/revoke", controller.PostIdentitiesRevoke)
   r.POST( "/identities/recover", controller.PostIdentitiesRecover)
 
-  r.Run() // listen and serve on 0.0.0.0:8080
+  r.RunTLS(":80", "/srv/certs/idpbe-cert.pem", "/srv/certs/idpbe-key.pem")
+  //r.Run() // listen and serve on 0.0.0.0:8080
 }
 
 func logRequest() gin.HandlerFunc {
