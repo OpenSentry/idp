6cba46b108ac1970d743b913afda16fe603392fe
Lasse Nielsen <charmixer@users.noreply.github.com>
2019-08-01 10:46:25 +0200

    Changed config to get rid of structs

--- config/config.go
+++ config/config.go
@@ -6,115 +6,21 @@ import (
   "strings"
 )
 
-type DiscoveryConfig struct {
-  IdpUi struct {
-    Public struct {
-      Url  string
-      Port string
-      Endpoints struct {
-      }
-    }
-  }
-  IdpApi struct {
-    Public struct {
-      Url  string
-      Port string
-      Endpoints struct {
-        Authenticate string
-        Identities string
-        Logout string
-      }
-    }
-  }
-  AapUi struct {
-    Public struct {
-      Url  string
-      Port string
-      Endpoints struct {
-      }
-    }
-  }
-  AapApi struct {
-    Public struct {
-      Url  string
-      Port string
-      Endpoints struct {
-        Authorizations string
-        AuthorizationsAuthorize string
-        AuthorizationsReject string
-      }
-    }
-  }
-  Hydra struct {
-    Public struct {
-      Url  string
-      Port string
-      Endpoints struct {
-        Oauth2Token string
-        Oauth2Auth string
-        Userinfo string
-        HealthAlive string
-        HealthReady string
-        Logout string
-      }
-    }
-    Private struct {
-      Url  string
-      Port string
-      Endpoints struct {
-        Consent string
-        ConsentAccept string
-        ConsentReject string
-        Login string
-        LoginAccept string
-        LoginReject string
-        Logout string
-        LogoutAccept string
-        LogoutReject string
-      }
-    }
-  }
-}
-
-type AppConfig struct {
-  Serve struct {
-    Public struct {
-      Port string
-    }
-    Tls struct {
-      Key struct {
-        Path string
-      }
-      Cert struct {
-        Path string
-      }
-    }
-  }
-  Neo4j struct {
-    Uri string
-    Username string
-    Password string
-  }
-  Csrf struct {
-    AuthKey string
-  }
-  Oauth2 struct {
-    Client struct {
-      Id string
-      Secret string
-    }
-    Scopes struct {
-      Required []string
-    }
-  }
-}
-
-var Discovery DiscoveryConfig
-var App AppConfig
-
 func setDefaults() {
-  viper.SetDefault("config.discovery.path", "./discovery.yml")
   viper.SetDefault("config.app.path", "./app.yml")
+  viper.SetDefault("config.discovery.path", "./discovery.yml")
+}
+
+func GetString(key string) string {
+  return viper.GetString(key)
+}
+
+func GetStringStrict(key string) string {
+  return viper.GetString(key)
+}
+
+func GetStringSlice(key string) []string {
+  return viper.GetStringSlice(key)
 }
 
 func InitConfigurations() {
@@ -134,21 +40,12 @@ func InitConfigurations() {
     panic(fmt.Errorf("Fatal error config file: %s \n", err))
   }
 
-  err = viper.Unmarshal(&Discovery)
-  if err != nil {
-    fmt.Printf("unable to decode into config struct, %v", err)
-  }
-
   // Load app specific configurations
 
   viper.SetConfigFile(viper.GetString("config.app.path"))
-  err = viper.ReadInConfig() // Find and read the config file
+  err = viper.MergeInConfig() // Find and read the config file
   if err != nil { // Handle errors reading the config file
     panic(fmt.Errorf("Fatal error config file: %s \n", err))
   }
 
-  err = viper.Unmarshal(&App)
-  if err != nil {
-    fmt.Printf("unable to decode into config struct, %v", err)
-  }
 }
