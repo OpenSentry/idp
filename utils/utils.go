f29c6c89f8770d571dfdd6dd21985201fdf1c1a1
Marc <wraix@users.noreply.github.com>
2019-09-20 09:02:53 +0200

    removed /public logs

--- utils/utils.go
+++ utils/utils.go
@@ -247,6 +247,11 @@ func RequestLogger(logKey string, requestIdKey string, log *logrus.Logger, appFi
       fullpath = path + "?" + raw
     }
 
+    // if public data is requested successfully, then dont log it since its just spam when debugging
+    if strings.Contains(path, "/public/") && ( statusCode == http.StatusOK || statusCode == http.StatusNotModified ) {
+     return
+    }
+
     log.WithFields(appFields).WithFields(logrus.Fields{
       "latency": latency,
       "forwarded_for.ip": forwardedForIpData.Ip,
