690d993fcc8fd8c8562cd756860084a8f0b8a616
Marc <wraix@users.noreply.github.com>
2019-10-10 08:49:26 +0200

    Added POST /clients

--- gateway/idp/events.go
+++ gateway/idp/events.go
@@ -7,5 +7,15 @@ import (
 
 func EmitEventHumanCreated(natsConnection *nats.Conn, human Human) {
   e := fmt.Sprintf("{id:%s, name:%s, email:%s}", human.Id, human.Name, human.Email)
-  natsConnection.Publish("idp", []byte(e))
+  natsConnection.Publish("idp.human.created", []byte(e))
+}
+
+func EmitEventHumanAuthenticated(natsConnection *nats.Conn, human Human) {
+  e := fmt.Sprintf("{id:%s}", human.Id)
+  natsConnection.Publish("idp.human.authenticated", []byte(e))
+}
+
+func EmitEventClientCreated(natsConnection *nats.Conn, client Client) {
+  e := fmt.Sprintf("{id:%s, name:%s}", client.Id, client.Name)
+  natsConnection.Publish("idp.client.created", []byte(e))
 }
