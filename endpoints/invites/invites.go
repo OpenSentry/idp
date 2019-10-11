8c05f7f5f6c731aeb6ecb8f22ca94beaac48eb00
Marc <wraix@users.noreply.github.com>
2019-10-11 08:50:27 +0200

    revamped invites ep to new model

--- endpoints/invites/invites.go
+++ endpoints/invites/invites.go
@@ -6,6 +6,7 @@ import (
   "github.com/sirupsen/logrus"
   "github.com/gin-gonic/gin"
 
+  "github.com/charmixer/idp/config"
   "github.com/charmixer/idp/environment"
   "github.com/charmixer/idp/gateway/idp"
   "github.com/charmixer/idp/client"
@@ -29,10 +30,24 @@ func PostInvites(env *environment.State) gin.HandlerFunc {
       return
     }
 
+    issuer := config.GetString("idp.public.issuer")
+    if issuer == "" {
+      log.Debug("Missing idp.public.issuer")
+      c.AbortWithStatus(http.StatusInternalServerError)
+      return
+    }
+
+    ttl := config.GetInt("invite.ttl")
+    if ttl <= 0 {
+      log.Debug("Missing invite.ttl. Hint: Invites that never expire are not supported.")
+      c.AbortWithStatus(http.StatusInternalServerError)
+      return
+    }
+
     invitedByIdentityId := c.MustGet("sub").(string)
     dbHumans, err := idp.FetchHumansById(env.Driver, []string{invitedByIdentityId})
     if err != nil {
-      log.Debug(err.Error())
+      log.WithFields(logrus.Fields{ "invited_by":invitedByIdentityId }).Debug(err.Error())
       c.AbortWithStatus(http.StatusInternalServerError)
       return
     }
@@ -47,38 +62,27 @@ func PostInvites(env *environment.State) gin.HandlerFunc {
       for _, request := range iRequests {
         r := request.Input.(client.CreateInvitesRequest)
 
-        var invited idp.Human
-        if r.Invited != "" {
-          dbHumans, err := idp.FetchHumansById(env.Driver, []string{r.Invited})
-          if err != nil {
-            log.Debug(err.Error())
-            request.Output = bulky.NewInternalErrorResponse(request.Index)
-            continue
-          }
-          if len(dbHumans) > 0 {
-            invited := dbHumans[0]
-            if invited.Email != r.Email {
-              request.Output = bulky.NewClientErrorResponse(request.Index, E.HUMAN_NOT_FOUND) // TODO: Make better error
-              continue
-            }
-          } else {
-            request.Output = bulky.NewClientErrorResponse(request.Index, E.HUMAN_NOT_FOUND)
-            continue
-          }
-
+        // Does indentity on email already exists?
+        dbHumans, err := idp.FetchHumansByEmail(env.Driver, []string{r.Email})
+        if err != nil {
+          log.Debug(err.Error())
+          request.Output = bulky.NewInternalErrorResponse(request.Index)
+          continue
         }
-
-        log.WithFields(logrus.Fields{"fixme": 1}).Debug("Put invite expiration into config")
+        if len(dbHumans) > 0 {
+          request.Output = bulky.NewClientErrorResponse(request.Index, E.HUMAN_ALREADY_EXISTS)
+          continue
+        }
 
         newInvite := idp.Invite{
           Identity: idp.Identity{
-            Issuer: "", // FIXME
-            ExpiresAt: time.Now().Unix() + (60 * 60 * 24), // 24 hours
+            Issuer: issuer,
+            ExpiresAt: time.Now().Unix() + int64(ttl),
           },
-          HintUsername: r.HintUsername,
-          Invited: invited,
+          Username: r.Username,
+          Email: r.Email,
         }
-        invite, _, _, err := idp.CreateInvite(env.Driver, newInvite, invitedBy, idp.Email{ Email:r.Email })
+        invite, _, err := idp.CreateInvite(env.Driver, newInvite, invitedBy)
         if err != nil {
           log.WithFields(logrus.Fields{ "invited_by": invitedBy.Id, "email": r.Email }).Debug(err.Error())
           request.Output = bulky.NewInternalErrorResponse(request.Index)
@@ -91,9 +95,8 @@ func PostInvites(env *environment.State) gin.HandlerFunc {
             Id: invite.Id,
             IssuedAt: invite.IssuedAt,
             ExpiresAt: invite.ExpiresAt,
-            Email: invite.SentTo.Email,
-            Invited: invite.Invited.Id,
-            HintUsername: invite.HintUsername,
+            Email: invite.Email,
+            Username: invite.Username,
             InvitedBy: invite.InvitedBy.Id,
           }
           request.Output = bulky.NewOkResponse(request.Index, ok)
@@ -167,9 +170,8 @@ func GetInvites(env *environment.State) gin.HandlerFunc {
               Id: i.Id,
               IssuedAt: i.IssuedAt,
               ExpiresAt: i.ExpiresAt,
-              Email: i.SentTo.Email,
-              Invited: i.Invited.Id,
-              HintUsername: i.HintUsername,
+              Email: i.Email,
+              Username: i.Username,
               InvitedBy: i.InvitedBy.Id,
             })
           }
