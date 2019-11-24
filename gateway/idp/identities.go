7af268f551219856737a414a487a2d7feeaa0815
Lasse Nielsen <65roed@gmail.com>
2019-11-24 19:06:41 +0100

    Added search and switched to call entities with scopes

--- gateway/idp/identities.go
+++ gateway/idp/identities.go
@@ -14,20 +14,21 @@ func FetchIdentities(tx neo4j.Transaction, iIdentities []Identity) (identities [
   var cypher string
   var params = make(map[string]interface{})
 
-  cypfilterIdentites := ""
+  cypFilterIdentities := ""
   if len(iIdentities) > 0 {
     var ids []string
     for _, identity := range iIdentities {
       ids = append(ids, identity.Id)
     }
-    cypfilterIdentites = ` WHERE i.id in split($ids, ",") `
+    cypFilterIdentities = ` AND i.id in split($ids, ",") `
     params["ids"] = strings.Join(ids, ",")
   }
 
   cypher = fmt.Sprintf(`
-    MATCH (i:Identity) %s RETURN i
-  `, cypfilterIdentites)
+    MATCH (i:Identity) WHERE 1=1 %s RETURN i
+  `, cypFilterIdentities)
 
+  logCypher(cypher, params)
   if result, err = tx.Run(cypher, params); err != nil {
     return nil, err
   }
@@ -43,7 +44,44 @@ func FetchIdentities(tx neo4j.Transaction, iIdentities []Identity) (identities [
     }
   }
 
+  // Check if we encountered any error during record streaming
+  if err = result.Err(); err != nil {
+    return nil, err
+  }
+
+  return identities, nil
+}
+
+func SearchIdentities(tx neo4j.Transaction, iSearch string) (identities []Identity, err error) {
+  var result neo4j.Result
+  var cypher string
+  var params = make(map[string]interface{})
+
+  cypFilterIdentities := ""
+  if iSearch != "" {
+    cypFilterIdentities = ` AND ( i.name =~ $search or i.email =~ $search)`
+    params["search"] = fmt.Sprintf(`(?i).*%s.*`, iSearch)
+  }
+
+  cypher = fmt.Sprintf(`
+    MATCH (i:Identity) WHERE 1=1 %s RETURN i
+  `, cypFilterIdentities)
+
+  if result, err = tx.Run(cypher, params); err != nil {
+    return nil, err
+  }
+
   logCypher(cypher, params)
+  for result.Next() {
+    record          := result.Record()
+    identityNode    := record.GetByIndex(0)
+
+    if identityNode != nil {
+      i := marshalNodeToIdentity(identityNode.(neo4j.Node))
+
+      identities = append(identities, i)
+    }
+  }
 
   // Check if we encountered any error during record streaming
   if err = result.Err(); err != nil {
